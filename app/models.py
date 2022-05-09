89fd4e60b75ca400326fb536880fae854fd709a0
rachel <rachelkemuma99@gmail.com>
2022-05-10 01:25:58 +0300

    add migration

--- app/models.py
+++ app/models.py
@@ -1,5 +1,13 @@
 from . import db
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
 class Pitch(db.Model):
     __tablename__ = 'pitches'
 
