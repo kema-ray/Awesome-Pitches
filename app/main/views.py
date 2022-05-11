33000f2d07da072ff3510d864d025e2f1bf6f4df
rachel <rachelkemuma99@gmail.com>
2022-05-11 08:04:54 +0300

    Creating a user profile and update section

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from unicodedata import category
-from flask import redirect, render_template,url_for
+from flask import redirect, render_template,url_for,request,abort
 from . import main
-from ..models import Pitch
-from .forms import PitchForm
-from flask_login import login_required
+from ..models import Pitch,User
+from .forms import PitchForm,UpdateProfile
+from flask_login import login_required,current_user
+from .. import db
 
 @main.route('/')
 def index():
@@ -53,6 +53,34 @@ def new_pitch():
          return redirect(url_for('.index'))
 
      return render_template('new_pitch.html',pitch_form=pitch_form)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
     
 
--- app/models.py
+++ app/models.py
@@ -8,11 +8,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
     email =  db.Column(db.String(255),unique=True,index=True)
-    password_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch',backref='user',lazy='dynamic')
+    password_secure = db.Column(db.String(255))
 
     @property
     def password(self):
